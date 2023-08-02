035de82e5c7af4a9ba1dee1ad3c93bcc86636fd7
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-02 17:48:04 +0300

    calculateDiscountedPrice & calculateTotalPrice & getFullName & their tests

---
+++ src/index.js
@@ -0,0 +1,47 @@
+
+// Task 1. Immutability and Pure Functions
+
+export function calculateDiscountedPrice(products, discount) {
+    if(!(products instanceof Array)) {
+        throw new TypeError("Products must be an array");
+    }
+    if(typeof discount !== "number" || Number.isNaN(discount)
+            || discount === Number.POSITIVE_INFINITY || discount === Number.NEGATIVE_INFINITY) {
+        throw new TypeError("Discount must be a number");
+    }
+    let productsWithDiscounts = [];
+    products.forEach(product => {
+        if (!product.hasOwnProperty('price')) {
+            throw new TypeError("Each product must contain price");
+        }
+        productsWithDiscounts.push({
+            name: product.name,
+            price: Number((product.price - product.price * discount / 100).toFixed(2))
+        })
+    });
+    return productsWithDiscounts;
+}
+
+export function calculateTotalPrice(products) {
+    if(!(products instanceof Array)) {
+        throw new TypeError("Products must be an array");
+    }
+    let totalPrice = 0;
+    products.forEach(product => {
+        if (!product.hasOwnProperty('price')) {
+            throw new TypeError("Each product must contain price");
+        }
+        totalPrice += product.price;
+    });
+    return Number(totalPrice.toFixed(2));
+}
+
+
+// Task 2: Function Composition and Point-Free Style
+
+export function getFullName(person) {
+    if (!person.hasOwnProperty('firstName') || !person.hasOwnProperty('lastName')) {
+        throw new TypeError("Person must have firstName and lastName");
+    }
+    return person.firstName + " " + person.lastName;
+}
---
+++ tests/index.test.js
@@ -0,0 +1,73 @@
+import { calculateDiscountedPrice, calculateTotalPrice, getFullName } from "../src/index";
+
+test('calculateDiscountedPrice', () => {
+    let given = [
+        { name: "Book", price: 42.67 },
+        { name: "Phone", price: 212.00 }
+    ];
+    let expected = [
+        { name: "Book", price: 40.54 },
+        { name: "Phone", price: 201.40 }
+    ];
+    let result = calculateDiscountedPrice(given, 5);
+    expect(result).toEqual(expected);
+    expect(given).toEqual([
+        { name: "Book", price: 42.67 },
+        { name: "Phone", price: 212.00 }
+    ]);
+});
+
+test('calculateDiscountedPrice with type errors', () => {
+    let productsWithNoPrice = [
+        { name: "Book", price1: 42.67 },
+        { name: "Phone"}
+    ];
+    expect(() => calculateDiscountedPrice(productsWithNoPrice, 5)).toThrow(TypeError);
+    expect(() => calculateDiscountedPrice([undefined, null, "product"], 5)).toThrow(TypeError);
+    expect(() => calculateDiscountedPrice(null, 5)).toThrow(TypeError);
+    expect(() => calculateDiscountedPrice([], NaN)).toThrow(TypeError);
+});
+
+test('calculateTotalPrice', () => {
+    let given = [
+        { name: "Book", price: 42.67 },
+        { name: "Phone", price: 212.00 }
+    ];
+    let expected = 254.67;
+    let result = calculateTotalPrice(given);
+    expect(result).toEqual(expected);
+    expect(given).toEqual([
+        { name: "Book", price: 42.67 },
+        { name: "Phone", price: 212.00 }
+    ]);
+});
+
+test('calculateTotalPrice with type errors', () => {
+    let productsWithNoPrice = [
+        { name: "Book", price1: 42.67 },
+        { name: "Phone"}
+    ];
+    expect(() => calculateTotalPrice(productsWithNoPrice)).toThrow(TypeError);
+    expect(() => calculateTotalPrice([undefined, null, "product"])).toThrow(TypeError);
+    expect(() => calculateTotalPrice(null)).toThrow(TypeError);
+});
+
+test('getFullName', () => {
+    let given = {
+        firstName: "Liza",
+        lastName: "Ya"
+    };
+    let expected = "Liza Ya";
+    let result = getFullName(given);
+    expect(result).toEqual(expected);
+});
+
+test('getFullName with type errors', () => {
+    let personWithBadProperties = {
+        myName: "Liza",
+        lastName2: "Ya"
+    };
+    expect(() => getFullName(personWithBadProperties)).toThrow(TypeError);
+    expect(() => getFullName(undefined)).toThrow(TypeError);
+    expect(() => getFullName("lallalala")).toThrow(TypeError);
+});

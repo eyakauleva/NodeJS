eabe1e5d5c54c9e20922c86ff60d4bf38ff3000f
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-03 17:39:17 +0300

    task2 functions refactored

--- src/index.js
+++ src/index.js
@@ -39,36 +39,71 @@ export function calculateTotalPrice(products) {
 // Task 2: Function Composition and Point-Free Style
 
 export function getFullName(person) {
-    if (!person.hasOwnProperty('firstName') || !person.hasOwnProperty('lastName')) {
+    if (isPersonValid(person)) {
+        return retrieveFullName(person);
+    }
+    else {
         throw new TypeError("Person must have firstName and lastName");
     }
+}
+
+function isPersonValid(person) {
+    return person.hasOwnProperty('firstName') && person.hasOwnProperty('lastName');
+}
+
+function retrieveFullName(person) {
     return person.firstName + " " + person.lastName;
 }
 
 export function filterUniqueWords(text) {
-    if(typeof text !== "string") {
+    isTextValid(text);
+    return text.toLowerCase()
+            .split(" ")
+            .filter(filterWords)
+            .sort();
+}
+
+function isTextValid(text) {
+    if (typeof text === "string") {
+        return true;
+    } else {
         throw new TypeError("Test must be a string");
     }
-    return text.toLowerCase()
-        .split(" ")
-        .filter((value, index, array) => array.indexOf(value) === index)
-        .sort();
+}
+
+function filterWords(value, index, array) {
+    return array.indexOf(value) === index;
 }
 
 export function getAverageGrade(students) {
+    areStudentsValid(students);
+    return gradesAverage(students);
+}
+
+function areStudentsValid(students) {
     if(!(students instanceof Array)) {
         throw new TypeError("Students must be an array");
     }
-    let gradesSum = 0, gradesCount = 0;
     students.forEach(student => {
         if (!student.hasOwnProperty('name') || !student.hasOwnProperty('grades') 
                 || !(student.grades instanceof Array)) {
             throw new TypeError("Each student must contain name and grades");
         }
-        gradesSum += student.grades.reduce((a, b) => a + b, 0);
+    });
+    return true;
+}
+
+function gradesAverage(students) {
+    let gradesSum = 0, gradesCount = 0;
+    students.forEach(student => {
+        gradesSum += student.grades.reduce(sumValues, 0);
         gradesCount += student.grades.length;
     });
     return Number((gradesSum / gradesCount).toFixed(1));
+}
+
+function sumValues(a, b) {
+    return a + b;
 }
 
 
@@ -129,9 +164,8 @@ export function power(base, exponent, prevResult = 1) {
 }
 
 function isNumberValid(number) {
-    if (typeof number !== "number" || Number.isNaN(number) 
-            || number === Number.POSITIVE_INFINITY || number === Number.NEGATIVE_INFINITY) {
-        return false;
-    }
-    return true;
+    return typeof number === "number" 
+            && !Number.isNaN(number) 
+            && number !== Number.POSITIVE_INFINITY 
+            && number !== Number.NEGATIVE_INFINITY;
 }

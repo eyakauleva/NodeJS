f7ff709ffd7802cb170853d717bd895f95e1b281
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-08 17:05:36 +0300

    array processing fix & properties configurable: false

--- src/task4.js
+++ src/task4.js
@@ -2,13 +2,18 @@ export function createImmutableObject (object) {
     if (typeof object !== "object" || object === null) {
         throw new TypeError("Param must be an object");
     }
-    const result = {};
-    Object.getOwnPropertyNames(object).forEach(property => {
+    let result;
+    if (object instanceof Array) {
+        result = [];
+    } else {
+        result = {};
+    }
+    Object.getOwnPropertyNames(object)
+    .filter(property => property !== "length")
+    .forEach(property => {
         let descriptor = Object.getOwnPropertyDescriptor(object, property);
         let newValue;
-        if (descriptor.value instanceof Array) {
-            newValue = processArray(descriptor.value);
-        } else if (typeof descriptor.value === "object" && descriptor.value !== null) {
+        if (typeof descriptor.value === "object" && descriptor.value !== null) {
             newValue = createImmutableObject(descriptor.value);
         } else {
             newValue = descriptor.value;
@@ -16,25 +21,9 @@ export function createImmutableObject (object) {
         Object.defineProperty(result, property, {
             value: newValue,
             writable: false,
-            enumerable: true
+            enumerable: true,
+            configurable: false
         });
     });
-    return result;
-}
-
-function processArray(array) {
-    let result = [];
-    array.filter(value => value !== "length").forEach(value => {
-        if (value instanceof Array) {
-            let innerArray = processArray(value);
-            Object.freeze(innerArray);
-            result.push(innerArray);
-        } else if (typeof value !== "object" || value === null) {
-            result.push(value);
-        } else {
-            result.push(createImmutableObject(value));
-        }
-    });
-    Object.freeze(result);
     return result;
 }

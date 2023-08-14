a00a81fdb0304f9f863f5e1108b5dfd9ddbaffb9
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-14 17:35:46 +0300

    partial func with underscore

--- src/task6.js
+++ src/task6.js
@@ -3,10 +3,23 @@ export function curry(func, arity) {
         throw new TypeError("Bad params");
     }
     let counter = 1;
-    return function curried (...args) {
+    return function curried(...args) {
         if (args.length != counter++) {
             throw new Error("Pass args one by one")
         }
+        if (arity !== args.length){
+            return curried.bind(null, ...args)
+        }
+        return func(...args);
+    };
+}
+
+export function partial(func, arity) {
+    if (typeof func !== "function" || typeof arity !== "number" || Number.isNaN(arity)) {
+        throw new TypeError("Bad params");
+    }
+    return function curried(...args) {
+        args = args.filter(arg => !(/^_$/.test(arg)))
         if (arity !== args.length){
             return curried.bind(null, ...args)
         }
--- test/task6.test.js
+++ test/task6.test.js
@@ -1,21 +1,44 @@
-import { curry } from "../src/task6";
+import { curry, partial } from "../src/task6";
 
 describe('task 6', () => {
+    const multiply = function (a, b, c) {  
+        return a * b * c;  
+    };
 
     test('curry ok', () => {
-        const multiply = function (a, b, c) {  
-            return a * b * c;  
-        } 
         const curriedMultiply = curry(multiply, 3);  
         const step1 = curriedMultiply(2);
         const step2 = step1(3);
         const result = step2(4);
+        expect(typeof curriedMultiply).toEqual("function");
+        expect(typeof step1).toEqual("function");
+        expect(typeof step2).toEqual("function");
         expect(result).toEqual(24);
     });
 
     test('curry validation', () => {
         expect(() => curry(undefined, 4)).toThrow(TypeError);
         expect(() => curry(() => {}, null)).toThrow(TypeError);
+    });
+
+    test('partial ok [no underscore]', () => {
+        const partialMultiply = partial(multiply, 3);  
+        const step1 = partialMultiply(2, 3);
+        const result = step1(4);
+        expect(result).toEqual(24);
+    });
+
+    test('partial ok [with underscore]', () => {
+        const partialMultiply = partial(multiply, 3);  
+        const step1 = partialMultiply(2, '_');
+        const step2 = step1(3);
+        const step3 = step2('_');
+        const result = step3(4);
+        expect(typeof partialMultiply).toEqual("function");
+        expect(typeof step1).toEqual("function");
+        expect(typeof step2).toEqual("function");
+        expect(typeof step3).toEqual("function");
+        expect(result).toEqual(24);
     });
 
 });

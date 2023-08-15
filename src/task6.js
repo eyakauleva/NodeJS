3adc70a2b92f92f029e01d8d491857a38b0650b4
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-15 15:35:08 +0300

    deepClone without recursion

--- src/task6.js
+++ src/task6.js
@@ -20,3 +20,45 @@ export function deepCloneObject(object) {
     });
     return result;
 }
+
+export function deepCloneWithoutRecursion(object) {
+    let queue = [], result = {};
+    Object.getOwnPropertyNames(object).forEach(property => {
+        queue.push(property);
+    });   
+    while(queue.length > 0) {
+        let propertyPath = queue.shift().split('.');
+        let parentObject = initObject, i = 0;
+        for (i = 0; i < propertyPath.length - 1; i++) {
+            parentObject = parentObject[propertyPath[i]];
+        }
+        let value = Object.getOwnPropertyDescriptor(parentObject, propertyPath[i]).value;
+        if (typeof value === "object" && value !== null && Object.keys(value).length !== 0) {
+            if (Array.isArray(value)) {
+                setValue(result, [], propertyPath);
+            } else {
+                setValue(result, {}, propertyPath);
+            }
+            Object.getOwnPropertyNames(value)
+                    .filter(property => property !== 'length')
+                    .forEach(property => {
+                        let fullPath = propertyPath.join('.') + '.' + property;
+                        queue.push(fullPath);
+                    });
+        } else {
+            setValue(result, value, propertyPath);
+        }
+    }
+    return result;
+}
+
+function setValue(obj, value, pathArray) {
+    let schema = obj, i = 0;
+    for (i = 0; i < pathArray.length - 1; i++) {
+        if(schema[pathArray[i]] === undefined) {
+            schema[pathArray[i]] = {};
+        }
+        schema = schema[pathArray[i]];
+    }
+    schema[pathArray[i]] = value;
+}

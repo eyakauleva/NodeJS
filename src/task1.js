676ec3addb5156d552e468fa662b87d02b06b7fa
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-08-16 10:38:27 +0300

    refactoring

--- src/task1.js
+++ src/task1.js
@@ -1,13 +1,11 @@
 export function localize(strings, ...keys) {
-    return (...values) => {
-        const language = values[0];
-        const dictionary = values[values.length - 1];
+    return (language, dictionary) => {
         if (typeof language !== "string" || typeof dictionary !== "object" || dictionary === null) {
             throw new TypeError("Bad params");
         }
         const result = [strings[0]];
         keys.forEach((key, i) => {
-            const value = Number.isInteger(key) ? values[key] : dictionary[language][key];
+            const value = dictionary[language][key];
             result.push(value, strings[i + 1]);
         });
         return result.join("");

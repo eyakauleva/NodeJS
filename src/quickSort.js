f644555168ea20b8b3b4733ff239fad4a2205f17
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-09-04 17:53:55 +0300

    quick sort

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,34 +1,18 @@
-function sort(array) {
-    if (!Array.isArray(array) || array.length == 0) {
-        throw new TypeError("Param must be a non-empty array");
+function sort(array, start, end) {
+    if (!Array.isArray(array) || !Number.isInteger(start) || !Number.isInteger(end)) {
+        throw new TypeError("Bad params");
     }
-    let length = array.length;
-    let pivot = array[length - 1], i = 0, j = -1;
-    for (; i < length; i++) {
-        if (array[i] < pivot || i === length - 1) {
+    if (start >= end) {
+        return array;
+    }
+    let pivot = array[end], j = start - 1;
+    for (let i = start; i <= end; i++) {
+        if (array[i] < pivot || i === end) {
             j++;
-            let buf = array[j];
-            array[j] = array[i];
-            array[i] = buf;
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
-    if (length > 2) { //TODO is ok?
-        let a = [], b = [];
-        if (j !== 0) {
-            a = sort(array.slice(0, j));
-        }
-        if (j !== length - 1) {
-            b = sort(array.slice(j + 1, length));
-        }
-        return a.concat(array[j]).concat(b);
-    }
-    else return array;
-    
+    sort(array, 0, j - 1);
+    sort(array, j + 1, end);
+    return array;
 }
-
-let arr = [8,5, -10,6,1,9,3,0,4, 2,8,7]; //TODO what if 1st el < ?
-console.log(sort(arr));
-// let b = [10,9];
-// let c = [-1];
-// console.log(arr.concat(b).concat(c))
-    
